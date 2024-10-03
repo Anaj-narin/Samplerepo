1ab43b48000de882ec2c5901a74f22644d23dd46
Anaj-narin <niranvelu2005@gmail.com>
2024-10-03 06:45:46 +0530

    feat: created Arithmetic file

---
+++ ArithmeticOperator.java
@@ -0,0 +1,18 @@
+
+public class ArithmeticOperator{
+    public static void main(String[] args)
+    {
+        int a =23;
+        int b =28;
+        int add=a+b;
+        int sub=a-b;
+        int mul=n1*n2;
+        int div=n1/n2;
+        int mod=n1%n2;
+        System.out.println("The add is:"+add);
+        System.out.println("The sub is:"+sub);
+        System.out.println("The mul is:"+mul);
+        System.out.println("The div is:"+div);
+        System.out.println("The mod is:"+mod);
+    }
+}
