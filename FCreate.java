cd8bfa522c221e49fc12bbb5dd74043855f6f02f
Anaj-narin <niranvelu2005@gmail.com>
2024-10-03 07:38:36 +0530

    created a file

---
+++ FCreate.java
@@ -0,0 +1,21 @@
+import java.io.File;
+import java.io.IOException;
+
+public class CreateFile {
+    public static void main(String[] args) {
+        try {
+           
+            File myFile = new File("example.txt");
+            
+    
+            if (myFile.createNewFile()) {
+                System.out.println("File created successfully");
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
