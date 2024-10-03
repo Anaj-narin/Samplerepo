34b40c798dd527c921c8da5c680ab402b0df0d8e
Anaj-narin <niranvelu2005@gmail.com>
2024-10-03 07:42:29 +0530

    read the file

---
+++ FRead.java
@@ -0,0 +1,22 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ReadFileExample {
+    public static void main(String[] args) {
+        try {
+     
+            File myFile = new File("example.txt");
+            Scanner myReader = new Scanner(myFile);
+            
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
